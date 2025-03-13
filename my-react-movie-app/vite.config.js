c3bc7c2fbcc3ce7af8ddda5904b0763ea43de8f0
nobantun <s219143943@mandela.ac.za>
2025-03-13 21:05:30 +0200

    fix

--- my-react-movie-app/vite.config.js
+++ my-react-movie-app/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  base: ProcessingInstruction.env.VITE_BASE_PATH || "/movies-app"
+  plugins: [react(),tailwindcss()]
 })
